42142a1a3a0cf046a3aee795203a2a818418cfaa
rayaanxsyed <157145884+rayaanxsyed@users.noreply.github.com>
2024-04-02 15:43:24 -0400

    Add files via upload

---
+++ Exercises/Ex12 - Final Remarks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("%s\n%s\n%s\n%s\n\n%s\n%s\n%s\n%s\n","I found the assignments in CSCA48 to be quite engaging while still testing the material for our units",
+                    "I liked the idea of the Wacky Store and working with real-life examples which allowed me to still familiarize myself with theoretical concepts like",
+                    "linked lists, graphs, and trees",
+                    "The exercises were also stimulating and I enjoyed making clever programs such as the sudoku solver and the exercise with find best professor using the knowledge of our course.",
+                    "In the Summer I plan to unwind a bit, spend time with my family, meet friends, go to the gym, and build some side projects!",
+                    "I will likely be taking 3 courses in the summer with a lighter schedule to stay in rhythm",
+                    "This course provided me a ton of knowledge in the programming world and I plan to continue learning and building my skills well into my career",
+                    "Thank you!");
+    return 0;
+}
